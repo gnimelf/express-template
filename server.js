9433ffeeb21b8960bd2a5811813bfb30c94e4c6f
justin <fezzygo@gmail.com>
2022-07-21 20:58:37 -0400

    added static middleware function to server client side html, images, css, and js, and a couple routes(server.js:9-49)

--- server.js
+++ server.js
@@ -6,14 +6,45 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-
-// Routes
-
-
+// use the following code to serve images, CSS files, and JavaScript files in a directory named public:
+app.use(express.static('public'));
 
 
+//  ROUTES
 
-// set server to listen for calls
+//  Middleware functions are functions that have access to the request object (req), 
+//  the response object (res), and the next function in the application’s request-response cycle. 
+
+//  Route definition: app.METHOD(PATH, HANDLER)
+//  METHOD:
+//  CREATE  -   POST method
+//  READ    -   GET method
+//  UPDATE  -   PUT method
+//  DELETE  -   DELETE method
+//  app is an instance of express.
+//  METHOD is an HTTP request method, in lowercase.
+//  PATH is a path on the server.
+//  HANDLER is the function executed when the route is matched.
+app.get("/response",(req,res)=>{
+    res.send("<h1>This is a get response!</h1>");
+});
+
+// GET params sent from client url
+app.get("/api/:firstParm", (req, res)=>{
+    if (req.params.firstParm === 'firstParam'){
+        res.json({
+            message: "hello world",
+        });
+    }else{
+        res.send("this is the wrong response")
+    }
+});
+
+app
+
+
+
+//SET SERVER TO LISTEN ON localhost ON GIVE A PORT;
 app.listen(port, ()=>{
-    console.log(`Server running @ http://localhost:${port}`)
+    console.log(`Server running @ http://localhost:${port}`);
 })
